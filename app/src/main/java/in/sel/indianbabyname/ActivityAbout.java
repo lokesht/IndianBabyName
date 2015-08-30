6d2e776d55eade45324933dfcc966da55a0ac499
Lokesh Tiwari <tiwari9773@gmail.com>
2015-08-31 02:06:03 +0530

    about page is complited now work is remaining on logo and splash screen

--- app/src/main/java/in/sel/indianbabyname/ActivityAbout.java
+++ app/src/main/java/in/sel/indianbabyname/ActivityAbout.java
@@ -1,13 +1,34 @@
 package in.sel.indianbabyname;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.widget.TextView;
 
-public class ActivityAbout extends Activity {
+public class ActivityAbout extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_about);
-	}
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        initialize();
+    }
+
+    public void initialize() {
+        mToolbar = (Toolbar) findViewById(R.id.tb_app_bar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextView textView = (TextView)findViewById(R.id.tv_about);
+        TextView textView2 = (TextView)findViewById(R.id.tv_about_2);
+        TextView tvThanx = (TextView)findViewById(R.id.tv_thax);
+        textView.setText(Html.fromHtml(getResources().getString(R.string.about_info)));
+        textView2.setText(Html.fromHtml(getResources().getString(R.string.about_below)));
+        tvThanx.setText(Html.fromHtml(getResources().getString(R.string.thanx)));
+
+    }
 }
